0ad60215c758b69d3146a8af39b479204a1c349b
pabby01 <adeolalasisi6@gmail.com>
2024-10-22 01:22:47 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Export the configuration for Vite
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
   server: {
-    port: 5173,
+    port: 5173, 
+    historyApiFallback: true, 
   },
 });
